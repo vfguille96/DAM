574809665f91e5ba045694020f58fd9357aa51c7
Guillermo Vera <vfguille@gmail.com>
2017-03-19 00:09:51 +0100

    Creación Menú, Excepción y Op. 1

--- PROG_REL8_EJ15_Vera_Guillermo/Program.cs
+++ PROG_REL8_EJ15_Vera_Guillermo/Program.cs
@@ -11,6 +11,9 @@ namespace PROG_REL8_EJ15_Vera_Guillermo
     {
         static void Main(string[] args)
         {
+
+            // Iniciamos el menú.
+            Menu();
         }
 
         static void Menu()
@@ -30,6 +33,9 @@ namespace PROG_REL8_EJ15_Vera_Guillermo
             Console.WriteLine("\n 9. Crear fichero.");
             Console.WriteLine();
             Console.WriteLine("\n\t 0. Salir del programa.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("\t -Seleccione una opción: ");
 
             switch (Console.Read())
             {
@@ -39,6 +45,8 @@ namespace PROG_REL8_EJ15_Vera_Guillermo
                     Console.WriteLine("   1. Alta de artículos.");
                     Console.WriteLine(" ========================= ");
                     Console.WriteLine();
+                    AltaArticulos();
+                    Console.ReadLine();
                     break;
                 case '2':
                     Console.WriteLine();
@@ -103,10 +111,137 @@ namespace PROG_REL8_EJ15_Vera_Guillermo
                     Console.WriteLine("   0. Salir del programa.");
                     Console.WriteLine(" ========================= ");
                     Environment.Exit(0);
-
                     break;
 
             }
+        }
+
+        static void AltaArticulos()
+        {
+            // Declaramos la ruta del fichero donde se guardan los artículos.
+            // Luego puede ser copiado al contenido de un fichero creado con la opción "9. Crear fichero".
+            string rutaAltaArticulos = Path.GetFullPath(@"AltaArticulos.txt");
+            
+            // Boolean todo ok
+            bool ok = true;
+
+            // Inicializamos el flujo.
+            StreamWriter flujoAltaArticulos = new StreamWriter(rutaAltaArticulos, ok);
+
+            // Declaramos.
+            int? cod_art = 000; // Entero, con posibilidad de null. Código de artículo.
+            string cod_artToString = ""; // String conversión de int.
+            string nombre_art = ""; // Nombre artículo.
+            float? precio = 0.00F; // Flotante, Precio artículo.
+            string precioToString = ""; // String de conversión de float.
+            float? pvp = 0.00F; // Flotante, PVP.
+            string pvpToString = ""; // String de conversión de float.
+            int existencias = 0; // Entero de existencias.
+            string existenciasToString = ""; // String conversión de int.
+            string comentario = "";
+
+
+
+
+            // Inicio del Método en consola.
+            Console.WriteLine(" Introduzca códigos de artículos.\n Si el valor introducido es '0', saldrá al menú.");
+            Console.WriteLine();
+            Console.ReadLine();
+            try
+            {
+                do // Escribe el contenido en el fichero siempre que el código no sea null.
+                {
+                    Console.WriteLine(" ================================================================== ");
+                    Console.WriteLine(" ================================================================== ");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    // Escribimos los cod_art.
+                    Console.Write("\n\t cod_art: ");
+                    cod_art = int.Parse(Console.ReadLine()); // Conversión para detectar int null.
+
+                    if (cod_art == 0)
+                    {
+                        throw new FormatException();
+                    }
+
+                    // Convertimos el número a string para escribirlo junto al nombre.
+                    cod_artToString = cod_art.ToString();
+                    
+                    // Escribimos los nombre_art.
+                    Console.Write("\n\t nombre_art: ");
+                    nombre_art = Console.ReadLine();
+
+                    if (nombre_art == string.Empty)
+                    {
+                        throw new FormatException();
+                    }
+
+                    // Escribimos el precio.
+                    Console.Write("\n\t precio: ");
+                    precio = float.Parse(Console.ReadLine());
+
+                    if (precio == 0)
+                    {
+                        throw new FormatException();
+                    }
+
+                    // Convertimos el número a string para escribirlo junto al nombre.
+                    precioToString = precio.ToString();
+
+                    // Escribimos el PVP.
+                    Console.Write("\n\t pvp: ");
+                    pvp = float.Parse(Console.ReadLine());
+
+                    if (pvp == 0)
+                    {
+                        throw new FormatException();
+                    }
+
+                    // Convertimos el número a string para escribirlo junto al nombre.
+                    pvpToString = pvp.ToString();
+
+                    // Escribimos las existencias.
+                    Console.Write("\n\t existencias: ");
+                    existencias = int.Parse(Console.ReadLine());
+
+                    // Convertimos el número a string para escribirlo junto al nombre.
+                    existenciasToString = existencias.ToString();
+
+                    // Comentario
+                    Console.Write("\n\t comentario: ");
+                    comentario = Console.ReadLine();
+
+                    Console.WriteLine();
+                    Console.WriteLine();
+
+                    // Escribimos la información anterior en el menú                   
+                    flujoAltaArticulos.Write(cod_artToString + " " + nombre_art + " " + precioToString + " " + existenciasToString + " " + comentario);
+                    flujoAltaArticulos.Close();
+
+                } while (cod_art != 0 && nombre_art != string.Empty && precio != 0 && pvp != 0);
+
+                Menu();
+
+            }
+            catch (Exception)
+            {
+                AltaArticulosException();
+            }
+        }
+
+        public static void AltaArticulosException()
+        {
+            // Limpia el caché de la consola.
+            Console.Clear();
+
+            // Devuelve por consola el siguiente mensaje.
+            Console.WriteLine("=========================================");
+            Console.WriteLine(" ERROR: FORMATO INTRODUCIDO INCORRECTO.");
+            Console.WriteLine("=========================================");
+            Console.WriteLine();
+            Console.WriteLine(" -El formato debe respetar 3 dígitos.");
+            Console.WriteLine(" -El nombre no puede estar vacío.");
+            Console.WriteLine(" -El precio del artículo no puede estar vacío o ser nulo.");
         }
     }
 }
