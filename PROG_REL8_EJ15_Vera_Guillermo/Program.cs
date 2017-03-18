a311c53d3ee3c5eef82dddbf48a1278eb0bdd976
Guillermo Vera <vfguille@gmail.com>
2017-03-18 14:20:09 +0100

    Creación de Menú.

--- PROG_REL8_EJ15_Vera_Guillermo/Program.cs
+++ PROG_REL8_EJ15_Vera_Guillermo/Program.cs
@@ -34,46 +34,76 @@ namespace PROG_REL8_EJ15_Vera_Guillermo
             switch (Console.Read())
             {
                 case '1':
+                    Console.WriteLine();
                     Console.WriteLine(" ========================= ");
                     Console.WriteLine("   1. Alta de artículos.");
                     Console.WriteLine(" ========================= ");
+                    Console.WriteLine();
                     break;
                 case '2':
+                    Console.WriteLine();
                     Console.WriteLine(" ========================= ");
                     Console.WriteLine("   2. Baja de artículos.");
                     Console.WriteLine(" ========================= ");
+                    Console.WriteLine();
                     break;
                 case '3':
+                    Console.WriteLine();
                     Console.WriteLine(" ============================= ");
                     Console.WriteLine("   3. Consultar un artículo.");
                     Console.WriteLine(" ============================= ");
+                    Console.WriteLine();
                     break;
                 case '4':
+                    Console.WriteLine();
                     Console.WriteLine(" ============================= ");
                     Console.WriteLine("   4. Modificar un artículo.");
                     Console.WriteLine(" ============================= ");
+                    Console.WriteLine();
                     break;
                 case '5':
+                    Console.WriteLine();
                     Console.WriteLine(" ============================================= ");
                     Console.WriteLine("   5. Listar artículos ordenados por código.");
                     Console.WriteLine(" ============================================= ");
+                    Console.WriteLine();
                     break;
                 case '6':
+                    Console.WriteLine();
                     Console.WriteLine(" ============================================= ");
-                    Console.WriteLine("   5. Listar artículos ordenados por código.");
+                    Console.WriteLine("   6. Listar artículos ordenados por nombre.");
                     Console.WriteLine(" ============================================= ");
+                    Console.WriteLine();
                     break;
                 case '7':
-                    Console.WriteLine("1. Alta de artículos.");
+                    Console.WriteLine();
+                    Console.WriteLine(" ================ ");
+                    Console.WriteLine("   7. Imprimir.");
+                    Console.WriteLine(" ================ ");
+                    Console.WriteLine();
                     break;
                 case '8':
-                    Console.WriteLine("1. Alta de artículos.");
+                    Console.WriteLine();
+                    Console.WriteLine(" ============================== ");
+                    Console.WriteLine("   8. Generar documento HTML.");
+                    Console.WriteLine(" ============================== ");
+                    Console.WriteLine();
                     break;
                 case '9':
-                    Console.WriteLine("1. Alta de artículos.");
+                    Console.WriteLine();
+                    Console.WriteLine(" ===================== ");
+                    Console.WriteLine("   9. Crear fichero.");
+                    Console.WriteLine(" ===================== ");
+                    Console.WriteLine();
                     break;
                 case '0':
-                    Console.WriteLine("1. Alta de artículos.");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine(" ========================= ");
+                    Console.WriteLine("   0. Salir del programa.");
+                    Console.WriteLine(" ========================= ");
+                    Environment.Exit(0);
+
                     break;
 
             }
