06b25b202d619632153f25067a0c47666485e80b
Guillermo Vera <vfguille@gmail.com>
2017-10-18 12:35:17 +0200

    Add files via upload

---
+++ App_JugandoConTipos_V1/App_JugandoConTipos_V1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_JugandoConTipos_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaramos
+            byte b1;
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine(" El tipo int es un \"{0}\"", typeof(int));
+            Console.WriteLine(" El tipo float es un \"{0}\"", typeof(float));
+            Console.WriteLine(" El tipo byte es un \"{0}\"", typeof(byte));
+            Console.WriteLine(" \n-------------------------------------------------------\n");
+            Console.WriteLine(" El tipo {0} ocupa {1} bytes en memoria.", "int", sizeof(int));
+            Console.WriteLine(" El tipo {0} ocupa {1} bytes en memoria.", "float", sizeof(float));
+            Console.WriteLine(" El tipo {0} ocupa {1} bytes en memoria.", "byte", sizeof(byte));
+            Console.WriteLine(" El tipo {0} ocupa {1} bytes en memoria.", "double", sizeof(double));
+            Console.WriteLine(" \n-------------------------------------------------------\n");
+            Console.WriteLine(" Los límites del tipo \"{0}\" son \"{1}\" y \"{2}\"", "int", int.MinValue, int.MaxValue);
+            Console.WriteLine(" Los límites del tipo \"{0}\" son \"{1}\" y \"{2}\"", "double", double.MinValue, double.MaxValue);
+            Console.WriteLine(" Los límites del tipo \"{0}\" son \"{1}\" y \"{2}\"", "sbyte", sbyte.MinValue, sbyte.MaxValue);
+            Console.WriteLine(" \n-------------------------------------------------------\n");
+            // Desbordamientos
+            b1 = 254;
+            Console.WriteLine(" El valor de {0} --> {1}", "b1", b1);
+            // No se usa!!!  b1 = b1 + 1;
+            b1++; // 255
+            //checked      Salta excepción. Con unchecked (igual que no ponerlo) no saltaría la excepción.
+            {
+                Console.WriteLine(" El valor de {0} --> {1}", "b1", b1);
+                b1++; // 256 -- El valor se vuelve a 0 porque el unchecked está activo. Vuelve al primer dígito.
+                Console.WriteLine(" El valor de {0} --> {1}", "b1", b1);
+            }
+
+            // Ejemplo de casting
+            int i1 = 65;
+            char letra = 'X';
+            letra = (char)int.Parse("65");  // Convertimos el string "65" en entero y luego le hacemos el casting a un char.
+            Console.WriteLine(" \n-------------------------------------------------------\n");
+            Console.WriteLine(" El ASCII de {0} es {1}", letra, i1);
+            Console.WriteLine(" El ASCII de {0} es {1}", letra, (int)i1);
+            Console.WriteLine(" El ASCII de {0} es {1}", (char)i1, i1);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ App_JugandoConTipos_V2/App_JugandoConTipos_V2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_JugandoConTipos_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaramos variables.
+            char c1 = (char)65; // Casting al 65 para convertirlo en caracter.
+            int i1 = 0;
+            byte b1 = (byte)'A';    // Casting al caracter 'A' para convertirlo a byte.
+            short s1 = 5;   // 0000 0000 0000 0101 en binario. 0005 en hexadecimal.
+            short s2 = 10;  // 0000 0000 0000 1010 en binario. 000A en hexadecimal.
+            string ss1 = "Hola";
+            string ss2 = "Hola";
+            int[] a1 = { 1, 2, 3 }; // Array que contiene los números "1, 2, 3" en la variable a1.
+            int[] a2 = { 1, 2, 3 };
+            string cadena = " ===> ";
+            float f1 = 3.141592F;   // Si se pone un número real, es doble. Si le digo al SO que sea flotante, hay que colocarle una F detrás.
+
+            Console.WriteLine(" El número s1 vale en decimal {0} y en hexadecimal {0:X4}", s1);
+            Console.WriteLine(" El número s1 vale en decimal {0} y en hexadecimal {0:X4}", s2);
+            Console.WriteLine();    // Salto de línea.
+            Console.WriteLine(" Escribe el complemento A1 de {0} es {1}", s2, ~s2);
+            Console.WriteLine(" Escribe el complemento A1 de {0:X8} es {1:X8} --> {2:X8}", s2, ~s2, ~~2);   // Sería 1111 1111 1111 0101 (FFFF FFFF FFFF 5). --> 0000002
+            Console.WriteLine();
+            Console.WriteLine(" Es ss1 == ss2, {0}", ss1 == ss2);   // Porque está sobrecargado el operador. Pero es por referencia.
+            Console.WriteLine(" Es a1 == a2, {0}", a1 == a2 ? "Verdad" : "Falso");  // Operador ternario condicional.
+            Console.WriteLine();
+            Console.WriteLine(" {0} es {1}", true, true);   // Introduce el valor booleano true en sus posiciones.
+            Console.WriteLine(" {0} es {1}", true, !true);  // Introduce el valor booleano correspondiente con la posición, pero a la posición {1} se le da la vuelta.
+            Console.WriteLine(" {0} es {1}", true, !!true);  // Introduce el valor booleano correspondiente con la posición, pero a la posición {1} se le da la vuelta dos veces.
+            Console.WriteLine(" {0} es {1}", b1 > s1, !!true);    // Devuelve el valor booleano de la operación de la posición {0} y luego una doble negación de una afirmación verdadera.
+
+            cadena += " Hola caracola\n" + ", " + " Un día vi una vaca...";
+            Console.WriteLine(cadena);
+
+            Console.ReadLine();
+        }
+    }
+}
